156580a5b54578547f5695b7761b020298b3c779
Mingh_x_Ghost <45220665+Mingh1112@users.noreply.github.com>
2023-03-20 15:17:05 +0530

    Update IDM.bat

--- IDM.bat
+++ IDM.bat
@@ -2,3 +2,4 @@
 REG DELETE HKEY_USERS\S-1-5-21-447685293-2759990947-948275436-1001_Classes\WOW6432Node\CLSID\{07999AC3-058B-40BF-984F-69EB1E554CA7} /f
 cd "C:\Program Files (x86)\Internet Download Manager\"
 start IDMan.exe
+echo Manish
