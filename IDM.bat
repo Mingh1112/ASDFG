fe60e366615fc85d20d963b679dee4a82eba5098
Mingh_x_Ghost <45220665+Mingh1112@users.noreply.github.com>
2023-03-20 15:16:29 +0530

    Add files via upload

---
+++ IDM.bat
@@ -0,0 +1,4 @@
+@echo off
+REG DELETE HKEY_USERS\S-1-5-21-447685293-2759990947-948275436-1001_Classes\WOW6432Node\CLSID\{07999AC3-058B-40BF-984F-69EB1E554CA7} /f
+cd "C:\Program Files (x86)\Internet Download Manager\"
+start IDMan.exe
